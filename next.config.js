e67e1ef532638873bdf2abe811a25553ceffa1e2
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-09-23 13:32:31 +0600

    api data load and show on UI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    images: {
+        remotePatterns: [
+            {
+            protocol: 'https',
+            hostname: 'komo.vamtam.com',
+            // hostname: 'https://imgbb.com/',
+            }
+        ]
+    },
 }
 
-module.exports = nextConfig
+
+module.exports = nextConfig;
--- src/Components/Home/OurBikes/OurBikes.jsx
+++ src/Components/Home/OurBikes/OurBikes.jsx
@@ -1,28 +1,72 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+// 'use client'
+// import React, { useEffect, useState } from 'react';
+import bikes from '../../../../public/bikes.json'
+import Image from 'next/image';
 import OurBikesDetails from '../OurBikesDetails/OurBikesDetails';
 
-const OurBikes = () => {
-    const [bikes, setBikes] = useState([]);
+const OurBikes = async() => {
+    // const [bikes, setBikes] = useState([]);
 
-    useEffect(() =>{
-        fetch('bikes.json')
-        .then(res => res.json())
-        .then(data => setBikes(data))
-    },[])
+    // const res = await fetch('bikes.json');
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    // const bikes = await res.json();
+
+    // useEffect(() =>{
+    //     // fetch('https://jsonplaceholder.typicode.com/users')
+    //     fetch('bikes.json')
+    //     .then(res => res.json())
+    //     .then(data => setBikes(data))
+    // },[])
     return (
         <section className='text-center'>
             <h2 className='text-3xl font-bold md:text-3xl lg:text-4xl'>Our Bikes</h2>
             <p>Which bike is the best for you? {bikes.length}</p>
 
-            {
+            {/* {
+                bikes.map(bike => <li key={bike.id}>{bike.title}</li>)
+                // bikes.map(bike => <li key={bike.id}>{bike.name}</li>)
+            } */}
+            {/* {
                 bikes.map(bike =>{
                     <OurBikesDetails
                     key={bike.id}
                     bike={bike}
                     ></OurBikesDetails>
                 })
+            } */}
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            {
+                bikes.map(bike =><>
+                {/* <section className="flex"> */}
+                <div className="card card-compact w-64 bg-base-100 shadow-xl">
+                    <figure><Image src={bike.image} width={200} height={100} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold">{bike.title}</h2>
+                        <div className='flex justify-between'>
+                            <div className='text-left'>
+                            <p>Hourly Rate</p>
+                            <p>Daily Rate</p>
+                            <p>Online</p>
+                            </div>
+                            <div className='text-left'>
+                                <p> {bike.hourlyRate}</p>
+                                <p> {bike.dailyRate}</p>
+                                <p> {bike.online}</p>
+                            </div>
+                        </div>
+                        <div className="card-actions justify-end">
+                        <button class="btn btn-primary btn-sm w-full bg-primary font-normal text-white text-lowercase">Book Now</button>
+
+                        {/* new  */}
+                        </div>
+                    </div>
+                </div>
+                {/* </section> */}
+                </>)
             }
+            </section>
+
+
         </section>
     );
 };
